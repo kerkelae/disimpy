0d97be5e211e6cd4362189fc1161b5339d8c0deb
kerkelae <leevi.kerkela@protonmail.com>
2020-05-19 14:30:58 +0100

    removed dipy dependency

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='disimpy',
     version='0.1',
-    description='Functions for Monte Carlo random walker dMRI simulations.',
+    description='Package for simulating diffusion MRI experiments using massively parallelized monte carlo random walker diffusion simulations.',
     url='https://github.com/kerkelae/disimpy',
     license='MIT',
     packages=['disimpy',],
-    install_requires=['numpy','numba','dipy','matplotlib','scipy'],
+    install_requires=['numpy','numba','matplotlib','scipy'],
 )

c2e225557fe6226f1b00a4868f49c13d63fdd3ed
kerkelae <leevi.kerkela@protonmail.com>
2020-05-30 18:16:52 +0100

    defined dependency versions

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from distutils.core import setup
 setup(
     name='disimpy',
     version='0.1',
-    description='Package for simulating diffusion MRI experiments using massively parallelized monte carlo random walker diffusion simulations.',
+    description='Package for simulating diffusion MRI experiments on CUDA GPU.',
     url='https://github.com/kerkelae/disimpy',
     license='MIT',
     packages=['disimpy',],
-    install_requires=['numpy','numba','matplotlib','scipy'],
+    install_requires=[
+        'numpy>=1.18.4',
+        'matplotlib>=3.1.3',
+        'numba>=0.49.0',
+        'scipy>=1.4.1',
+        'pytest>=5.4.1',
+    ],
 )

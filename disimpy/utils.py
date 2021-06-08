341fd7b3bedee9a87894d0ad17d6d7476ac64e8c
Leevi Kerkela <leevi.kerkela@protonmail.com>
2021-06-08 11:54:45 +0200

    added function from old meshes module and improved unit tests

--- disimpy/utils.py
+++ disimpy/utils.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
 def vec2vec_rotmat(v, k):
@@ -20,6 +21,8 @@ def vec2vec_rotmat(v, k):
     R : numpy.ndarray
         3 by 3 rotation matrix.
     """
+    v = v / np.linalg.norm(v)
+    k = k / np.linalg.norm(k)
     axis = np.cross(v, k).astype(np.float)
     if np.linalg.norm(axis) < np.finfo(float).eps:
         if np.linalg.norm(v - k) > np.linalg.norm(v):
@@ -67,6 +70,50 @@ def show_traj(traj_file, show=True):
                 trajectories[:, i, 1],
                 trajectories[:, i, 2],
                 alpha=.5)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.ticklabel_format(style='sci', scilimits=(0, 0))
+    if show:
+        plt.show()
+    else:
+        plt.close(fig)
+    return
+
+
+def show_mesh(mesh, show=True):
+    """Visualize the triangular mesh with random triangle colours.
+
+    Parameters
+    ----------
+    mesh : numpy.ndarray
+        Mesh array of shape (n of triangles, 3, 3) where the second dimension
+        indices correspond to different triangle points and the third dimension
+        is the Cartesian coordinates of triangle.
+    show : bool
+        Whether to render figure or not.
+
+    Returns
+    -------
+    None
+    """
+    np.random.seed(123)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    mesh_min = np.min(np.min(mesh, 0), 0)
+    mesh_max = np.max(np.max(mesh, 0), 0)
+    ax.set_xlim([mesh_min[0], mesh_max[0]])
+    ax.set_ylim([mesh_min[1], mesh_max[1]])
+    ax.set_zlim([mesh_min[2], mesh_max[2]])
+    for triangle in mesh:
+        A = triangle[0, :]
+        B = triangle[1, :]
+        C = triangle[2, :]
+        vtx = np.array([A, B, C])
+        tri = Poly3DCollection([vtx], alpha=.5)
+        face_color = np.random.random(3)
+        tri.set_facecolor(face_color)
+        ax.add_collection3d(tri)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')

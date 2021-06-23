8c855b39af9b354470b8cee464a55894a1d9a4e5
Leevi Kerkela <leevi.kerkela@protonmail.com>
2021-06-23 18:21:10 +0200

    updated epsilon

--- disimpy/settings.py
+++ disimpy/settings.py
@@ -3,10 +3,10 @@
 # The maximum number of allowed iterations in the intersection check algorithm
 MAX_ITER = 1e4
 
-# This constant defines the proportion of the step length by which a random
-# walker's position is separated from the surface after a collision to avoid
-# placing the walker in the surface.
-EPSILON = 1e-7
+# This constant defines the distance by which a random walker's position is
+# separated from the surface after a collision to avoid placing the walker in
+# the surface.
+EPSILON = 1e-14
 
 # The gyromagnetic ratio of the simulated spin
 GAMMA = 267.513e6

51e7cbc7880a9f9eb80f2c351c98f67b8890e304
kerkelae <leevi.kerkela@protonmail.com>
2021-10-16 16:58:15 +0100

    formatted with black

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='disimpy',
-    version='0.1.2',
-    description='Massively parallel diffusion MR simulator',
-    url='https://github.com/kerkelae/disimpy',
-    license='MIT',
-    packages=['disimpy', 'disimpy.tests'],
+    name="disimpy",
+    version="0.1.2",
+    description="Massively parallel diffusion MR simulator",
+    url="https://github.com/kerkelae/disimpy",
+    license="MIT",
+    packages=["disimpy", "disimpy.tests"],
     install_requires=requirements,
     include_package_data=True,
-    package_data={'': ['tests/*']},
+    package_data={"": ["tests/*"]},
 )
